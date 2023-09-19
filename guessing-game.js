43e0051e5018c9099b82edd64f504c8f50040ab3
markmazurk <markmazurk@gmail.com>
2023-09-19 15:21:54 -0700

    guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,23 @@
 function guessingGame() {
+  let randomNumber = Math.floor(Math.random() * 100);
+  let count = 1;
+  let gameOver = false;
 
+  function game(number) {
+    if (gameOver) {
+      return "The game is over, you already won!";
+    } else if (number > randomNumber) {
+      count++;
+      return `${number} is too high!`;
+    } else if (number < randomNumber) {
+      count++;
+      return `${number} is too low!`;
+    } else if (number === randomNumber) {
+      gameOver = true;
+      return `You win! You found ${randomNumber} in ${count} guesses.`;
+    }
+  }
+  return game;
 }
 
 module.exports = { guessingGame };
